8e1c64c3ce608d7bf00afc4c82e2324fd481f175
Terrell Rountree <terrellrountree415@gmail.com>
2021-04-09 08:12:14 -0400

    updated main page

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <head>
 <title>Sleepy Fairy Doula</title>
 <link rel="stylesheet" type="text/css" href="style.css">
@@ -11,27 +11,95 @@
 <meta name="keywords" content="Sleepy Fairy Doula, doula, birth, baby, postpartum, newborn, pregnancy, prenatal">
 </head>
 <body>
-<div id="menubars" class="container"><div class="col-sm-4"><img src="menubars.png" />
-<nav id="mainnav">
-	<ul>
-		<li><a href="#">Meet Royan</a></li>
-		<li><a href="#">Doula?</a></li>
-		<li><a href="#">Services</a></li>
-		<li><a href="#">Testimonials and Reviews</a></li>
-		<li><a href="#">Stories</a></li>
-		<li><a href="#">Blog</a></li>
-		<li><a href="#">Diary</a></li>
-		<li><a href="#">Calendar</a></li>
-		<li><a href="#">Coming Soon</a></li>
-		<li><a href="#">Social</a></li>
-		<li><a href="#">Contact</a></li>
-	</ul>
-</nav></div>
-<div id="headerdiv" class="col-sm-11"><h1>Sleepy Fairy Doula</h1></div></div>
-<div class="container"><div class="col-sm-2"></div>
-<div id="content" class="col-sm-8">A doula (/ˈduːlə/) is a trained companion who is not a healthcare professional and who supports another individual (the doula's client) through a significant health-related experience, such as childbirth, miscarriage, induced abortion or stillbirth, or non-reproductive experiences such as dying. A doula may also provide support to the client's partner, family, and friends. The doula's goal, and role, is to help the client feel safe and comfortable, complementing the role of the healthcare professionals who provide the client's medical care.
+	<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">WebSiteName</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Home</a></li>
+      <li><a href="#">Page 1</a></li>
+      <li><a href="#">Page 2</a></li>
+      <li><a href="#">Page 3</a></li>
+    </ul>
+  </div>
+</nav>
+<nav class="nav navbar-default">
+	<div class="container-fluid">
+		<div class="navbar-header">
+			<a class="navbar-brand" href="#">Sleepy Fairy Doula</a>
+		</div>
+		<ul class="nav navbar-nav">
+			<li><a href="#">Meet Royan</a></li>
+			<li><a href="#">Doula?</a></li>
+			<li><a href="#">Services</a></li>
+			<li><a href="#">Testimonials and Reviews</a></li>
+			<li><a href="#">Stories</a></li>
+			<li><a href="#">Blog</a></li>
+			<li><a href="#">Diary</a></li>
+			<li><a href="#">Calendar</a></li>
+			<li><a href="#">Coming Soon</a></li>
+			<li><a href="#">Social</a></li>
+			<li><a href="#">Contact</a></li>
+		</ul>
+	</div>
+</nav>
+<div class="container">
+	<div class="col-sm-2"></div>
+	<div id="content" class="col-sm-8 text-center">A doula (/ˈduːlə/) is a trained companion who is not a healthcare professional and who supports another individual (the doula's client) through a significant health-related experience, such as childbirth, miscarriage, induced abortion or stillbirth, or non-reproductive experiences such as dying. A doula may also provide support to the client's partner, family, and friends. The doula's goal, and role, is to help the client feel safe and comfortable, complementing the role of the healthcare professionals who provide the client's medical care.
 
-Unlike a physician, midwife, or nurse, a doula cannot administer medication or other medical treatment or give medical advice. An individual may need to complete training to work as a doula, although trainings and certification processes vary throughout the world. Some doulas work as volunteers; others are paid for their services by their client, medical institutions, or other private and public organizations. Doulas receive varying amounts of training, and their professionalization also varies. The contributions of doulas during reproductive experiences and end-of-life care have been studied and have been shown to benefit their clients. For example, a birth doula providing support during childbirth may increase likelihood of vaginal birth (rather than cesarean delivery), decrease the need for pain medication during labor, and improve the perception of the birthing experience. The benefits of a doula providing other types of support have been less well studied, but might improve a client's experience with medical care or help an individual cope with health transitions.</div>
-<div class="col-sm-2"></div></div>
+Unlike a physician, midwife, or nurse, a doula cannot administer medication or other medical treatment or give medical advice. An individual may need to complete training to work as a doula, although trainings and certification processes vary throughout the world. Some doulas work as volunteers; others are paid for their services by their client, medical institutions, or other private and public organizations. Doulas receive varying amounts of training, and their professionalization also varies. The contributions of doulas during reproductive experiences and end-of-life care have been studied and have been shown to benefit their clients. For example, a birth doula providing support during childbirth may increase likelihood of vaginal birth (rather than cesarean delivery), decrease the need for pain medication during labor, and improve the perception of the birthing experience. The benefits of a doula providing other types of support have been less well studied, but might improve a client's experience with medical care or help an individual cope with health transitions.
+</div>
+<div class="col-sm-2"></div>
+</div>
 <footer></footer>
+</body> -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Sleepy Fairy Doula</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Sleepy Fairy Doula commercial page">
+	<meta name="keywords" content="Sleepy Fairy Doula, doula, birth, baby, postpartum, newborn, pregnancy, prenatal">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<nav class="nav navbar-default">
+	<div class="container-fluid">
+		<div class="navbar-header">
+			<a class="navbar-brand" href="#">Sleepy Fairy Doula</a>
+		</div>
+		<ul class="nav navbar-nav">
+			<li><a href="#">Meet Royan</a></li>
+			<li><a href="#">Doula?</a></li>
+			<li><a href="#">Services</a></li>
+			<li><a href="#">Testimonials and Reviews</a></li>
+			<li><a href="#">Stories</a></li>
+			<li><a href="#">Blog</a></li>
+			<li><a href="#">Diary</a></li>
+			<li><a href="#">Calendar</a></li>
+			<li><a href="#">Coming Soon</a></li>
+			<li><a href="#">Social</a></li>
+			<li><a href="#">Contact</a></li>
+		</ul>
+	</div>
+</nav>
+  
+<div class="container">
+	<div class="col-sm-2"></div>
+	<div id="content" class="col-sm-8 text-center">
+		A doula (/ˈduːlə/) is a trained companion who is not a healthcare professional and who supports another individual (the doula's client) through a significant health-related experience, such as childbirth, miscarriage, induced abortion or stillbirth, or non-reproductive experiences such as dying. A doula may also provide support to the client's partner, family, and friends. The doula's goal, and role, is to help the client feel safe and comfortable, complementing the role of the healthcare professionals who provide the client's medical care.
+		Unlike a physician, midwife, or nurse, a doula cannot administer medication or other medical treatment or give medical advice. An individual may need to complete training to work as a doula, although trainings and certification processes vary throughout the world. Some doulas work as volunteers; others are paid for their services by their client, medical institutions, or other private and public organizations. Doulas receive varying amounts of training, and their professionalization also varies. The contributions of doulas during reproductive experiences and end-of-life care have been studied and have been shown to benefit their clients. For example, a birth doula providing support during childbirth may increase likelihood of vaginal birth (rather than cesarean delivery), decrease the need for pain medication during labor, and improve the perception of the birthing experience. The benefits of a doula providing other types of support have been less well studied, but might improve a client's experience with medical care or help an individual cope with health transitions.
+	</div>
+	<div class="col-sm-2"></div>
+</div>
+<footer></footer>
+
 </body>
+</html>
